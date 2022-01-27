035ebf0668fb52624f9dcae934d70c47af677ab7
Oleg <olvor196@gmail.com>
2022-01-28 00:53:04 +0100

    LAB2

--- 002_Samples/src/main/java/LAB2/Bank.java
+++ 002_Samples/src/main/java/LAB2/Bank.java
@@ -1,15 +1,40 @@
 package LAB2;
 
-import java.util.List;
+import lombok.Data;
 
+import java.util.List;
+import java.util.logging.Logger;
+
+@Data
 public class Bank {
     private List<Client> clients;
+    private String name;
+    private Logger LOG;
+
+    public Bank() {
+    }
+
+    public Bank(List<Client> clients, String name, Logger LOG) {
+        this.clients = clients;
+        this.name = name;
+        this.LOG = LOG;
+    }
 
     public boolean issueLoan(Client client, double loan) {
-        return (loan * 12.0 + loan / 10.0) <= client.getSum();
+        boolean result = (loan * 12.0 + loan / 10.0) <= client.getSum();
+        String sumsInfo = ", loan = " + loan + ", client sum = " + client.getSum();
+        LOG.info((result ? "Loan issued to the client " + client.getName() + sumsInfo
+                : "Loan was not issued to the client " + client.getName() + sumsInfo
+        ));
+        return result;
     }
 
     public boolean issueDeposit(Client client, double deposit) {
-        return deposit >= 1000 && client.getSum() >= deposit;
+        boolean result = deposit >= 1000 && client.getSum() >= deposit;
+        String sumsInfo = ", deposit = " + deposit + ", client sum = " + client.getSum();
+        LOG.info((result ? "Deposit issued to the client " + client.getName() + sumsInfo + "\n"
+                : "Deposit was not issued to the client " + client.getName() + sumsInfo
+                + "\n"));
+        return result;
     }
 }
--- 002_Samples/src/main/java/LAB2/Client.java
+++ 002_Samples/src/main/java/LAB2/Client.java
@@ -1,5 +1,8 @@
 package LAB2;
 
+import lombok.Data;
+
+@Data
 public class Client {
     private String name;
     private double sum;
@@ -10,29 +13,5 @@ public class Client {
     public Client(String name, double sum) {
         this.name = name;
         this.sum = sum;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSum() {
-        return sum;
-    }
-
-    public void setSum(double sum) {
-        this.sum = sum;
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" +
-                "name='" + name + '\'' +
-                ", sum=" + sum +
-                '}';
     }
 }
